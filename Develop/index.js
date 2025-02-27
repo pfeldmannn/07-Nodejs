e599ae92ce7c27b76253c6807766380b4a03e42f
Paige Feldmann <paigeafeldmann@gmail.com>
2025-02-26 20:34:01 -0600

    latest changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,10 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer'; 
+import fs, { read } from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
 // TODO: Create an array of questions for user input
-const questions = [
+const promptUser = () => {
+    return inquirer.prompt([
     {
         type: 'input',
         message: 'What is the title of your project?',
@@ -10,39 +13,71 @@ const questions = [
     {
         type: 'input',
         message: 'Please provide a description of your project:',
-        name: 'entitled Description',
+        name: 'entitledDescription',
     },
     {
         type: 'input',
         message: 'Please provide installation instructions:',
-        name: 'Installation',
+        name: 'installation',
     },
     {
         type: 'input',
         message: "Please provide usage information:",
-        name: 'Usage',
+        name: 'usage',
     },
     {
         type: 'input',
         message: 'What are your contribution guidelines?',
-        name: 'Contributing',
+        name: 'contributing',
     
     },
     {
         type: 'input',
         message: 'What are your test instructions?',
-        name: 'Tests',  
+        name: 'tests',  
     },
     {
-        type: 
+        name: 'license',
+        type: 'list',
+        message: 'What license would you like to use?',
+        choices: [
+            'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License','Boost Software License 1.0', 'The Unlicense'
+        ],
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
     }
-];
+    ])
+    .then((data) => {
+        const fileName = generateMarkdown(data);
+    });
+
+};
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, (err) =>
+        err ? console.log(err) : console.log('Successfuly created README.md')
+    );
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+        .then((data) => writeToFile('README.md',generateMarkdown(data)))
+        .then(() => console.log('Successfully created README.md'))
+        .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,38 +1,104 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'License:',
-      choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License','Boost Software License 1.0', 'The Unlicense'],
-    },
-  ])
-  .then((response) =>
-    console.log(
-      colors[response.license](`${response.license}`)
-    )
-};
+  const GNUAGPLv3 = 'GNU AGPLv3';
+  const GNUGPLv3 = 'GNU GPLv3';
+  const GNULGPLv3 = 'GNU LGPLv3';
+  const mozillaPublicLicense2 = 'Mozilla Public License 2.0';
+  const apacheLicense2 = 'Apache License 2.0';
+  const MITLicense = 'MIT License';
+  const boostSoftwareLicense1 = 'Boost Software License 1.0';
+  const theUnlicense = 'The Unlicense';
 
+  switch (license) {
+    case GNUAGPLv3:
+      return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]`;
+    case GNUGPLv3:
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+    case GNULGPLv3:
+      return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPLv3-blue.svg)]`
+    case mozillaPublicLicense2:
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]`
+    case apacheLicense2:
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
+    case MITLicense:
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+    case boostSoftwareLicense1:
+      return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]`
+    case theUnlicense:
+      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+    default:
+      return '';
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  switch (license) {
+    case 'GNU AGPLv3':
+      return 'https://www.gnu.org/licenses/agpl-3.0';
+    case 'GNU GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'GNU LGPLv3':
+      return 'https://www.gnu.org/licenses/lgpl-3.0';
+    case 'Mozilla Public License 2.0':
+      return 'https://opensource.org/licenses/MPL-2.0';
+    case 'Apache License 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'MIT License':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Boost Software License 1.0':
+      return 'https://www.boost.org/LICENSE_1_0.txt';
+    case 'The Unlicense':
+      return 'https://choosealicense.com/licenses/unlicense/';
+    default:
+      return '';
+}};
   
-}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if (license) {
+    return `##License
+    ${data.license}`
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  `# ${data.title} ${renderLicenseBadge(data.license)}
 
-`;
+  ## Description
+  ${data.entitledDescription}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+  ${data.installation}
+
+  ##Usage
+  ${data.usage}
+
+  ##Credits
+
+  ##License
+  ${data.license}
+
+  ##Contributing
+  ${data.contributing}
+
+  ##Tests
+  ${data.tests}`
+
 }
 
 export default generateMarkdown;
